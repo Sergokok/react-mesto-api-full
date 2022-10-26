779654e023012687cf8ddfeea6617c2fad36b9d1
sergokok <voronokins@yandex.ru>
2022-10-26 22:33:18 +0300

    кое что поменял

--- backend/app.js
+++ backend/app.js
@@ -15,7 +15,7 @@ const { celebrate, Joi, errors } = require('celebrate');
 const auth = require('./middlewares/auth');
 const { createUser, login, logout } = require('./controllers/users');
 const NotFoundError = require('./errors/not-found-err');
-const ServerError = require('./errors/server-err');
+// const ServerError = require('./errors/server-err');
 
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 
@@ -64,23 +64,30 @@ app.use(limiter);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
+
 app.use(express.json());
 
 app.use(requestLogger);
 app.use(errorLogger);
 
-async function start(req, res, next) {
-  try {
-    await mongoose.connect('mongodb://localhost:27017/mestodb', {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    await app.listen(PORT);
-  } catch (err) {
-    next(new ServerError('Ошибка сервера'));
-  }
-}
-start();
+// подключаемся к серверу mongo по-другому
+mongoose.connect('mongodb://localhost:27017/mestodb', {
+  useNewUrlParser: true,
+});
+app.listen(PORT);
+
+// async function start(req, res, next) {
+//   try {
+//     await mongoose.connect('mongodb://localhost:27017/mestodb', {
+//       useNewUrlParser: true,
+//       useUnifiedTopology: true,
+//     });
+//     await app.listen(PORT);
+//   } catch (err) {
+//     next(new ServerError('Ошибка сервера'));
+//   }
+// }
+// start();
 
 // роут для авторизации выхода
 app.get('/logout', logout);
